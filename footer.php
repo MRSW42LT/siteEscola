e5a1ead291a64e4ef372cb43296d56ae9547f9cc
denisborbasianhaki <81716552+denisborbasianhaki@users.noreply.github.com>
2021-08-30 08:48:52 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,1 +1,26 @@
-
+<footer>
+            <div class="container_footer">
+                <ul class="formas-de-pagamento">
+                    <li><i class="fa fa-bitcoin" style="font-size:36px"></i></li>
+                    <li><i class="fa fa-cc-amex" style="font-size:36px"></i></li>
+                    <li><i class="fa fa-cc-diners-club" style="font-size:36px"></i></li>
+                    <li><i class="fa fa-cc-mastercard" style="font-size:36px"></i></li>
+                    <li><i class="fa fa-cc-paypal" style="font-size:36px"></i></li>
+                    <li><i class="fa fa-google-wallet" style="font-size:36px"></i></li>
+                    <li><i class="fa fa-cc-jcb" style="font-size:36px"></i></li>
+                    <li><i class="fa fa-cc-discover" style="font-size:36px"></i></li>
+                </ul><hr>
+                <ul class="social">
+                    <form action="">
+                    <input type="text" placeholder="Search.." name="">
+                    <button type="" id="search-button"><i class="fa fa-search"  style="font-size:16px"></i></button>
+                    <li><a href="https://www.facebook.com/"><i class="fa fa-facebook" style="font-size:26px"></i></a></li>
+                    <li><a href="https://twitter.com/"><i class="fa fa-twitter" style="font-size:26px"></i></a></li>
+                    <li><a href="https://www.instagram.com/"><i class="fa fa-instagram" style="font-size:26px"></i></a></li>
+                    <li><a href="https://github.com/denisborbasianhaki"><i class="fa fa-github" style="font-size:26px"></i></a></li>
+                    <li><a>Chris_Careca © 2021</a></li>
+                </ul>
+            </div>
+        </footer>
+    </body>
+</html>
