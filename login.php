c0b11c0d6a018270724da9c8ef354cc875543307
denisborbasianhaki <81716552+denisborbasianhaki@users.noreply.github.com>
2021-08-30 09:07:11 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php include('header.php'); ?>
+
+<form action="" method="post" class="login-main-container">
+            </div>
+  
+            <div class="container">
+                <label for="uname"><b>Username</b></label>
+                <input type="text" placeholder="Enter Username" name="uname" required>
+  
+                <label for="psw"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="psw" required>
+          
+                <button type="submit">Login</button>
+                <label>
+                <input type="checkbox" checked="checked" name="remember"> Remember me
+                </label>
+            </div>
+                <span class="psw2"><a href="#">Register</a></span>
+                <span class="psw">Forgot <a href="#">password?</a></span>
+        </form>
+
+
+        
+<?php include('footer.php'); ?>
+
---
+++ sobre.php
@@ -0,0 +1,20 @@
+<?php include('header.php'); ?>
+
+<main class="sobre-main-container">
+            <h1 id="title">Sobre a empresa</h1>
+            <div id="content">
+                <p>Nossa barbearia foi fundada em 1917, nas trincheiras da Primeira Guerra Mundial, após nosso fundador perceber que todos estavam ficando barbudos e cabeludos, ele decidiu abrir uma barbearia no campo de batalha, ajudando assim, a moral e o visual dos combatentes.</p>
+                <div id="links"><hr>
+                    <p>Procurando por outra coisa?</p>
+                    <ul>
+                        <li><a href="">Relação com investidores</a></li>
+                        <li><a href="">Contato</a></li>
+                        <li><a href="">Impacto</a></li>
+                        <li><a href="">Doações</a></li>
+                        <li><a href="">Ajuda</a></li>
+                    </ul>
+                </div>
+            </div>
+        </main>
+
+<?php include('footer.php'); ?>
