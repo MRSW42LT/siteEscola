94e6d1235f87d6dd85dd9736a3a6b91054c60b52
denisborbasianhaki <81716552+denisborbasianhaki@users.noreply.github.com>
2021-08-30 08:48:40 -0300

    Update header.php

--- header.php
+++ header.php
@@ -1,1 +1,18 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name=“viewport” content=“width=device-width, initial-scale=1.0”>
+    <meta http-equiv=“X-UA-Compatible” content=“ie=edge”>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Chriscareca - Barbearia</title>
+    <meta http-equiv='cache-control' content='no-cache'>
+    <meta http-equiv='expires' content='0'>
+    <meta http-equiv='pragma' content='no-cache'>
+</head>
+    <body>
+        <header>
+            <a href="https://chriscareca.tk/" id="header-left">Chris Careca Guns Shop & Co.</a>
+            <a href="login" id="header-right"><i class="fa fa-sign-in" style="font-size:14px"></i> Entrar</a>
+            <a href="sobre" id="header-right">Sobre</a>
+        </header>
