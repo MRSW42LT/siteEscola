cddc76a657f45b2ca475da15b18785521b8205fb
denisborbasianhaki <81716552+denisborbasianhaki@users.noreply.github.com>
2021-08-30 08:49:32 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,65 @@
 <?php include('header.php'); ?>
+
+
+
+
 <main>
+      <div class="imagem-top">
+        <div class="retangulo-top">
+            <p id="informacao-titulo">Our Path Forward</p>
+                    <p id="informacao-conteudo">Doação de munições para treinamento de policias.</p>
+                        <button class="button">Ler Mais</button>
+        </div>
+    </div>
+    <div class="cardstitle">Nossas principais a armas de fogo<hr></div>
+    <div class="row">
+        <div class="column">
+          <div class="card">
+            <img src="glockg44.png" alt="GLOCK G44 Compact Semi-Auto Pistol" style="width:100%">
+            <h1>GLOCK G44 Compact Semi-Auto Pistol</h1>
+            <p class="price">$399.99</p>
+            <p><button>Adicionar ao carrinho</button></p>
+          </div>
+          <div class="card">
+            <img src="rugersp101.png" alt="Ruger SP101 Double-Action Revolver with Adjustable Sight" style="width:100%">
+            <h1>Ruger SP101 Double-Action Revolver with Adjustable Sight</h1>
+            <p class="price">$699.99</p>
+            <p><button>Adicionar ao carrinho</button></p>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <img src="waltherp22q.png" alt="Walther P22Q Semi-Auto Rimfire Pistol" style="width:100%">
+            <h1>Walther P22Q Semi-Auto Rimfire Pistol</h1>
+            <p class="price">$299.99</p>
+            <p><button>Adicionar ao carrinho</button></p>
+          </div>
+          <div class="card">
+            <img src="fn509.png" alt="FN 509 Tactical Semi-Auto Pistol in FDE with Vortex Viper Micro Red Dot Sight Package" style="width:100%">
+            <h1>FN 509 Tactical Semi-Auto Pistol in FDE with Red Dot Sight Package</h1>
+            <p class="price">$999.99</p>
+            <p><button>Adicionar ao carrinho</button></p>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <img src="beretta92fs.png" alt="Beretta 92FS Rimfire Pistols" style="width:100%">
+            <h1>Beretta 92FS Semi-Auto Rimfire Pistols</h1>
+            <p class="price">$419.99</p>
+            <p><button>Adicionar ao carrinho</button></p>
+          </div>
+          <div class="card">
+            <img src="mp9.png" alt="Smith & Wesson M&P9 Shield Plus Semi-Auto Pistol" style="width:100%">
+            <h1>Smith & Wesson M&P9 Shield Plus Semi-Auto Pistol Thumb Safety</h1>
+            <p class="price">$549.99</p>
+            <p><button>Adicionar ao carrinho</button></p>
+          </div>
+        </div>
+    </div>
 </main>
+
+
+
+
 <?php include('footer.php'); ?>
 
